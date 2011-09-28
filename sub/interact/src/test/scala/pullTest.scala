ca29deea66daae39aeb60e4ba6e7e31ec3e03288
laedracl <laedracl@gmaill.com>
2011-09-28 22:07:13 +0200

    Switch from FlatSpec to FreeSpec - allow better reuse of common test code

--- sub/interact/src/test/scala/pullTest.scala
+++ sub/interact/src/test/scala/pullTest.scala
@@ -1,11 +1,11 @@
 package me.home.tools.afrps
 
-import org.scalatest.FlatSpec
 import org.scalatest.matchers.MustMatchers
 import org.scalatest.matchers.BePropertyMatcher
 import org.scalatest.matchers.BePropertyMatchResult
+import org.scalatest.FreeSpec
 
-class PullTest extends FlatSpec with MustMatchers {
+class PullTest extends FreeSpec with MustMatchers {
   def anInstanceOf[T](implicit manifest: Manifest[T]) = {
     val clazz = manifest.erasure.asInstanceOf[Class[T]]
     new BePropertyMatcher[AnyRef] {
@@ -14,77 +14,101 @@ class PullTest extends FlatSpec with MustMatchers {
   }
 
   import SF._
-  "The arr of a function f: {i:Int => i.toString}" must "be SF[Int, String]" in {
+  "The arr of a function f: {i:Int => i.toString} must be SF[Int, String]" - {
     arr { i: Int => i.toString } must be(anInstanceOf[SF[Int, String]])
   }
 
-  "return" must "be a function" in {
-    `return`(10) must be(anInstanceOf[Function0[Int]])
+  "The return of a value must" - {
+    val r = `return`(10)
+    "be a function" - {
+      r must be(anInstanceOf[Function0[Int]])
+    }
+
+    "provide access to its constant argument" - {
+      r() must be(10)
+    }
+
+    "provide access to its variable argument" - {
+      var a: Int = 0;
+
+      val r = `return`(a);
+
+      r() must be(0)
+      a = 10
+      r() must be(10)
+    }
   }
 
-  it must "provide access to its constant argument" in {
-    `return`(10)() must be(10)
+  "The >>> of two sf's must" - {
+    "sequence the operations of two sf's (both mutable and immutable)" - {
+      val sf = arr { i: Int => i.toString } >>> arr { s: String => s + s } // Signal function level
+
+      // Immutable
+      val s = sf(`return`(10)) // Signal level
+      s() must be("1010")
+
+      // Mutable
+      var a = 0
+      val s_var = sf(`return`(a))
+      s_var() must be("00")
+      a = 123
+      s_var() must be("123123")
+    }
   }
 
-  it must "provide access to its variable argument" in {
-    var a: Int = 0;
-
-    val r = `return`(a);
-
-    r() must be(0)
-    a = 10
-    r() must be(10)
-  }
-
-  ">>>" must "sequence the operations of two sf's (both mutable and immutable)" in {
-    val sf = arr { i: Int => i.toString } >>> arr { s: String => s + s } // Signal function level
-
-    // Immutable
-    val s = sf(`return`(10)) // Signal level
-    s() must be("1010")
-
-    // Mutable
-    var a = 0
-    val s_var = sf(`return`(a))
-    s_var() must be("00")
-    a = 123
-    s_var() must be("123123")
-  }
-
-  "&&&" must "parallelize the operations of two sf's into a tuple (both mutable and immutable)" in {
+  "The &&& of two sf's must" - {
     val sf = arr { i: Int => i.toString } &&& arr { i: Int => i * 100 }
-
-    // Immutable
-    val s = sf(`return`(10))
-    s() must be(("10", 1000))
-
-    var a = 0
-    val s_var = sf(`return`(a))
-    s_var() must be(("0", 0))
-    a = 111
-    s_var() must be(("111", 11100))
+    "process a return of a val in parallel" - {
+      val s = sf(`return`(10))
+      s() must be(("10", 1000))
+    }
+    "process a return of a var in parallel" - {
+      var a = 0
+      val s_var = sf(`return`(a))
+      s_var() must be(("0", 0))
+      a = 111
+      s_var() must be(("111", 11100))
+    }
   }
 
-  "first" must "be a Function0 of a Tuple" in {
-    val sf = (arr { i: Int => i / 100 } &&& arr { i: Int => i * 100 }) >>> first(arr { i: Int => i.toString })
+  "first must" - {
+    "be a Function0 of a Tuple" - {
+      val sf = (arr { i: Int => i / 100 } &&& arr { i: Int => i * 100 }) >>> first(arr { i: Int => i.toString })
 
-    var a = 1000
-    val s = sf(`return`(a))
+      var a = 1000
+      val s = sf(`return`(a))
 
-    s() must be(("10", 100000))
+      s() must be(("10", 100000))
+    }
   }
-  
-  "loop" must "create a SF[A, B] from a SF2[A, B, C] and a C" in {
+
+  "loop must" - {
     val sf2 = arr { t: (Int, Int) => (t._1 + t._2, t._2 + 1) }
-    val sf = sf2.loop(0) // Defines the scope of the state!
-    sf must be (anInstanceOf[SF[Int, Int]])
-    var a = 10
-    val s = sf(`return`(a))
-    s() must be (10)
-    s() must be (11)
-    a = 0
-    
-    s() must be (2)
-    sf(`return`(a))() must be (3) // Check if state gets reset by recreating s...
+    "create a SF[A, B] from a SF2[A, B, C] and a C" - {
+      val sf = sf2.loop(0)
+      sf must be(anInstanceOf[SF[Int, Int]])
+    }
+    "act as a stateful function" - {
+      val sf = sf2.loop(0) // Defines the scope of the state!
+      "with a return of val" - {
+        sf must be(anInstanceOf[SF[Int, Int]])
+        val s = sf(`return`(10))
+        s() must be (10)
+        s() must be (11)
+      }
+
+      var a = 10
+      "with a return of var" - {
+        val s = sf(`return`(a))
+        s() must be(12)
+        s() must be(13)
+
+        a = 0
+        s() must be(4)
+      }
+      "which keeps its state when a new signal is created" - {
+        sf(`return`(a))() must be(5) 
+      }
+    }
   }
 }
